8bfd9f8e0edecb820c11f5962ece860c51788eac
Naeem Ashraf <123843315+NP5555@users.noreply.github.com>
2025-02-05 20:01:03 +0500

    Update Navbar component layout and comment out unused Link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,12 +2,12 @@ import Link from 'next/link'
 
 export default function Navbar() {
   return (
-    <header className="flex h-20 items-center justify-between px-6">
+    <header className="flex h-20 items-center justify-center px-6">
       <Link href="/">
         <h2 className="text-2xl font-semibold">wagmi-prototype-v1</h2>
       </Link>
       <div className="flex gap-6 text-xl">
-        <Link
+        {/* <Link
           href="/connect"
           className="px-3 py-1 duration-200 hover:bg-zinc-800/40"
         >
@@ -24,7 +24,7 @@ export default function Navbar() {
           className="px-3 py-1 duration-200 hover:bg-zinc-800/40"
         >
           Write
-        </Link>
+        </Link> */}
       </div>
     </header>
   )
